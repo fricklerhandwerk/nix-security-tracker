eae768686d4f6738ad9d66a4fd0599cb480a8f64
Valentin Gagarin <valentin.gagarin@tweag.io>
2026-02-10 09:45:37 +0100

    chore: garbage-collect orphaned derivation metadata

--- src/shared/management/commands/garbage_collect_derivations.py
+++ src/shared/management/commands/garbage_collect_derivations.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from itertools import chain
 from typing import Any
 
 from django.core.management.base import BaseCommand
@@ -25,6 +26,8 @@ class Command(BaseCommand):
         # FIXME(@fricklerhandwerk): This assumes derivations are deleted when their metadata is deleted, which is the case at the time of writing.
         # Derivations should instead be protected, but currently we're not deduplicating any metadata.
         # Fix this logic when derivation metadata is deduplicated!
+        # FIXME(@fricklerhandwerk): This orphans `NixDerivationOutput` row, which are currently already 100x redundant.
+        # It's impractical to garbage-collect them without copying, so the fix would be to rewire all live derivations in a migration, and drop that table.
         metas = NixDerivation.objects.filter(
             parent_evaluation__state__in=[
                 NixEvaluation.EvaluationState.CRASHED,
@@ -35,13 +38,21 @@ class Command(BaseCommand):
             # and we're not creating any new problematic instances.
         ).values_list("metadata", flat=True)
 
-        self.stdout.write("Querying derivations from crashed evaluations...")
+        orphaned = NixDerivationMeta.objects.filter(
+            derivation__isnull=True
+        ).values_list("id", flat=True)
+
+        all_metas = chain(metas, orphaned)
+
+        self.stdout.write("Querying derivations from crashed and failed evaluations...")
 
         batch_size = options["batch_size"]
-        total = metas.count()
-        metas_list = list(metas)
+        metas_list = list(all_metas)
+        total = len(metas_list)
 
-        self.stdout.write(f"Found {total} derivations")
+        self.stdout.write(
+            f"Found {metas.count()} derivations and {orphaned.count()} orphaned metadata entries"
+        )
 
         for i in range(0, total, batch_size):
             batch = metas_list[i : i + batch_size]
